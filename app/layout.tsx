a60988f750d96ff09d41baf2f714f047e846e5b5
vishvesh khanvilkar <18201051vishesh@viva-technology.org>
2024-04-02 22:51:22 +0530

    lamding page

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { icons } from "lucide-react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Motion",
+  description: "The connected workspace where better , faster work happens.",
+  icons:{
+    icon:[
+      {
+        media:"(prefers-color-scheme: light)",
+        url:"/logo.svg",
+        href:"/logo.svg",
+      },
+      {
+        media:"(prefers-color-scheme: dark)",
+        url:"/logo-dark.svg",
+        href:"/logo-dark.svg",
+      }
+    ]
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
