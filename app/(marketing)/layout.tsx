be517de734fc19d849e913579efd349ce9b371ac
vishvesh khanvilkar <18201051vishesh@viva-technology.org>
2024-04-05 20:52:17 +0530

    adding convex

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,12 @@
-import { Navbar } from "./Navbar";
+import { Navbar } from "./_components/navbar";
+
+
 
 const MarketingLayout = ({
     children
-}:{
+}: {
     children: React.ReactNode;
-})=>{
+}) =>{
     return (  
         <div className="h-full dark:bg-[#1f1f1f]">
             <Navbar/>
